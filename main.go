62faf4c1a505ea6b47dd2b9fa3390b0b8c4ab09b
Sam Newby <sam.newby@tails.com>
2022-11-25 17:40:14 +0000

    s33

--- main.go
+++ main.go
@@ -58,9 +58,20 @@ func getCursorPosition(rows *int, cols *int) int {
 
 		buf = append(buf, buffer[0])
 	}
-	fmt.Printf("\r\n%q\r\n", string(buf[1:]))
-	editorReadKey()
-	return -1
+	if string(buf[0:2]) != "\x1b[" {
+		log.Printf("failed to read rows or cols from tty\n")
+		return -1
+	}
+	if n, e := fmt.Sscanf(string(buf[2:]), "%d;%d", rows, cols); n != 2 || e != nil {
+		if e != nil {
+			log.Printf("get cursor position: fmt.Sscanf() failed: %s\n", e)
+		}
+		if n != 2 {
+			log.Printf("get cursor porition: got %d items, wanted 2\n", n)
+		}
+		return -1
+	}
+	return 0
 }
 
 func getWindowSize(rows *int, cols *int) int {
