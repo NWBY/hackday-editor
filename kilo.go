3456e1e75fc68a88a844c470a848838e4023966b
Sam Newby <sam.newby@tails.com>
2022-11-25 11:22:18 +0000

    s3

--- kilo.go
+++ kilo.go
@@ -3,5 +3,9 @@ package main
 import "os"
 
 func main() {
+	buffer := make([]byte, 1)
+	for cc, err := os.Stdin.Read(buffer); err == nil && cc == 1; cc, err = os.Stdin.Read(buffer) {
+		// ignore
+	}
 	os.Exit(0)
 }
