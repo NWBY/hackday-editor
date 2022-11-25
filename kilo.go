96a67f6460c4fbc809c722c653cca821a79187be
Sam Newby <sam.newby@tails.com>
2022-11-25 11:25:52 +0000

    s4

--- kilo.go
+++ kilo.go
@@ -4,7 +4,7 @@ import "os"
 
 func main() {
 	buffer := make([]byte, 1)
-	for cc, err := os.Stdin.Read(buffer); err == nil && cc == 1; cc, err = os.Stdin.Read(buffer) {
+	for cc, err := os.Stdin.Read(buffer); buffer[0] != 'q' && err == nil && cc == 1; cc, err = os.Stdin.Read(buffer) {
 		// ignore
 	}
 	os.Exit(0)
